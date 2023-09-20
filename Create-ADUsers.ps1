9bb7cc11075bd81c3014253ea0c987225a7417ed
infinity-set <infinity-set>
2023-09-20 14:57:08 -0500

    Local repo commit

---
+++ Create-ADUsers.ps1
@@ -0,0 +1,138 @@
+﻿# Define a function to prompt the user to specify whether the user(s) should be enabled or disabled.
+function Get-UserEnabledStatus {
+    while ($true) {
+        Write-Host ""
+        $response = Read-Host "Do you want the user(s) enabled (Y/N)?"
+        if ($response -eq "Y" -or $response -eq "y") {
+            return $true
+        } elseif ($response -eq "N" -or $response -eq "n") {
+            return $false
+        } else {
+            Write-Host ""
+            Write-Host "Invalid response. Please enter 'Y' for enabled or 'N' for disabled."
+        }
+    }
+}
+
+# Define a function to check if a file exists at the specified path.
+function Find-File {
+    param (
+        [string]$FilePath
+    )
+
+    if (Test-Path -Path $FilePath -PathType Leaf) {
+        # File exists, continue
+    } else {
+        Write-Host ""
+        Write-Host "File '$FilePath' not found." -BackgroundColor Black -ForegroundColor Red
+        Write-Host ""
+        Write-Host "Exiting..."
+        exit
+    }
+}
+
+# Define a function to test the complexity of an Active Directory password.
+function Test-ADPasswordComplexity {
+    param (
+        [string]$Password
+    )
+
+    # Define complexity requirements
+    $minLength = 8
+    $requireUppercase = $true
+    $requireLowercase = $true
+    $requireDigit = $true
+    $requireSpecialCharacter = $true
+
+    # Check password length
+    if ($Password.Length -lt $minLength) {
+        return $false
+    }
+
+    # Check for uppercase character
+    if ($requireUppercase -and $Password -notmatch "[A-Z]") {
+        return $false
+    }
+
+    # Check for lowercase character
+    if ($requireLowercase -and $Password -notmatch "[a-z]") {
+        return $false
+    }
+
+    # Check for digit
+    if ($requireDigit -and $Password -notmatch "[0-9]") {
+        return $false
+    }
+
+    # Check for special character (you can customize the character class as needed)
+    if ($requireSpecialCharacter -and $Password -notmatch "[@#$%^&+=!]") {
+        return $false
+    }
+
+    # Password meets complexity requirements
+    return $true
+}
+
+# Main script starts here
+
+# Display introductory messages
+Write-Host ""
+Write-Host "This script will add users from a CSV File"
+sleep 2
+Write-Host ""
+
+# Get file path
+$FilePath = Read-Host "Type the relative or absolute path of your CSV file"
+
+# Check if file path exists
+Find-File -FilePath $FilePath
+
+# Prompt the user for a secure password, confirm password, and ensure it meets complexity requirements
+Write-Host ""
+Write-Host "First, select a password for your new users..."
+sleep 2
+
+while ($true) {
+    try {
+        Write-Host ""
+        $pw1 = Read-Host "Select a secure password for these users" -AsSecureString
+        $pw2 = Read-Host "Confirm the password" -AsSecureString
+
+        # Convert secure strings to plain text
+        $pw1PlainText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pw1))
+        $pw2PlainText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pw2))
+
+        if ($pw1PlainText -ne $pw2PlainText) {
+            Write-Host ""
+            Write-Host "Passwords do not match. Please try again." -BackgroundColor Black -ForegroundColor Red
+            continue
+        } else {
+            if (Test-ADPasswordComplexity -Password $pw1PlainText) {
+                Write-Host ""
+                Write-Host "Password meets complexity requirements." -BackgroundColor Black -ForegroundColor Green
+                break
+            } else {
+                Write-Host "Password does not meet complexity requirements." -BackgroundColor Black -ForegroundColor Red
+                continue
+            }
+        }
+    } catch {
+        Write-Host ""
+        Write-Host "An error occurred: $_" -BackgroundColor Black -ForegroundColor Red
+        Write-Host "Exiting..."
+        exit
+    }
+}
+
+# Determine whether the user(s) should be enabled or disabled
+$isEnabled = Get-UserEnabledStatus
+
+# Import user data from a CSV file and create new AD users
+try {
+    Import-Csv ".\users.csv" | New-ADUser -AccountPassword $pw1 -ChangePasswordAtLogon $true -Enabled $isEnabled
+} catch {
+    Write-Host ""
+    Write-Host "An error occurred while creating user accounts: $_" -BackgroundColor Black -ForegroundColor Red
+    Write-Host ""
+    exit
+}
--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# ActiveDirectory_UserCreation_Script
+# Active Directory User Creation Script
+
+## Overview
+
+Welcome to the Active Directory User Creation Script! This PowerShell script simplifies the process of creating user accounts in Active Directory from a CSV file. Whether you need to add multiple users with specific attributes or automate user provisioning, this tool can help streamline the process.
+
+Read on to learn how to effectively use the script and understand its benefits.
+
+## Table of Contents
+
+- [Why Use This Script](#why-use-this-script)
+- [Languages and Utilities Used](#languages-and-utilities-used)
+- [Environments Used](#environments-used)
+- [Prerequisites](#prerequisites)
+- [Instructions](#instructions)
+- [Script Structure](#script-structure)
+- [License](#license)
+- [Contributions](#contributions)
+
+## Why Use This Script
+
+- **Efficient User Creation**: This script allows you to create user accounts in Active Directory quickly and accurately, especially when adding multiple users at once.
+
+- **Customization**: You can customize the CSV file to include user attributes like department, display name, and more, making it adaptable to your specific needs.
+
+- **Password Complexity Check**: The script includes a password complexity check to ensure that user passwords meet security requirements.
+
+- **Enable/Disable Users**: You can choose whether to enable or disable user accounts during creation, providing flexibility in user account management.
+
+## Languages and Utilities Used
+
+- **PowerShell**
+
+[<img alt="PowerShell Icon" width="50px" src="https://raw.githubusercontent.com/PowerShell/PowerShell/master/assets/ps_black_64.svg?sanitize=true" />][powershell]
+
+[powershell]: https://learn.microsoft.com/en-us/powershell/
+
+## Environments Used
+
+- **Windows**
+
+[<img align="left" alt="Microsoft Icon" width="35px" src="https://upload.wikimedia.org/wikipedia/commons/3/34/Windows_logo_-_2012_derivative.svg" />][windows]
+
+[windows]: https://www.microsoft.com/
+
+<br /><br />
+
+## Prerequisites
+
+Before using this script, ensure you have the following:
+
+- Active Directory module installed on your computer.
+- Sufficient permissions to create user accounts in Active Directory.
+
+## Instructions
+
+Follow these steps to use the script:
+
+1. **Clone or Download**: Clone or download this repository to your local machine.
+
+2. **Prepare CSV File**: Example: Create a CSV file with the following columns for user attributes:
+
+   - SamAccountName
+   - Name
+   - GivenName
+   - Surname
+   - DisplayName
+   - Department
+
+   You can add additional attributes as needed.
+
+3. **Run the Script**: Open PowerShell, navigate to the script directory, and run the script using the following command:
+
+   ```powershell
+   .\Create-ADUsers.ps1
+
+   ```
+
+4. **Follow Prompts:**: The script will prompt you for the CSV file path, a secure password for user accounts, and whether to enable or disable user accounts.
+
+5. **User Account Creation:** Navigate to Active Directory Users and Computers to view your new users.
